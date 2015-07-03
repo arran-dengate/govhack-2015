c8e688c3a511b3bf09e1999e0f27c342ba990d46
Arran Dengate <arran.dengate@gmail.com>
2015-07-03 22:22:03 +1000

    Made the script web scrape text from the listed pages.

--- scripts/importer.py
+++ scripts/importer.py
@@ -1,6 +1,31 @@
 __author__ = 'arran'
 
-# Import the CSV file and download all the text in it.
+# Imports the JSON file and does stuff.
 
 print("Hello world")
+
+import json
+import urllib2
+import re
+
+# Import file
+
+file = json.loads(open("data.json").read(), encoding="cp1252")
+
+with open("output", "w") as outfile:
+    json.dump(file, outfile, indent=4)
+
+# Read each line and go scrape the website.
+
+for index, line in enumerate(file):
+    url = file[index]["URL"]
+    response = urllib2.urlopen(url)
+    html = response.read()
+    text = re.findall("<p>.*</p>", html)
+    for index, paragraph in enumerate(text):
+        # Get rid of HTML tags.
+        text[index] = re.sub("\<.{1,6}\>", "", paragraph)      # Remove tags
+        text[index] = re.sub("\<a href=.{1,200}\>", "", text[index]) # Remove hyperlinks (might del other stuff, could improve)
+        #print(paragraph)
+    print(text)
 
