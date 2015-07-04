dfc6a2c41c845dc457f6a6975b5225a39d2711dd
Rebecca Dengate <rebecca.dengate@gmail.com>
2015-07-04 22:29:49 +1000

    working with split datasets now, including by keyword

--- nationalmap/wwwroot/images/dupe_test_data.py
+++ nationalmap/wwwroot/images/dupe_test_data.py
@@ -6,14 +6,14 @@ import os
 import csv
 
 def writeToFile(fn, lines):
-    #if len(lines) > 1000:
-    #    lines = lines[0:1000]
+    if len(lines) > 200:
+        lines = lines[0:200]
     with open(fn, 'w') as f:
         f.writelines(lines)
 
 base_path = "/Users/rdengate/Desktop/govhack_2015/govhack-2015/nationalmap/wwwroot"
 #csv_base_path = os.path.join(base_path, "abc_photojournalism.csv")
-csv_base_path = os.path.join(base_path, "abc_photojournalism.csv")
+csv_base_path = os.path.join(base_path, "words-sanitised.csv")
 csv_with_billboards = os.path.join(base_path, "abc_photojournalism_cp.csv")
 done_csv_nsw = os.path.join(base_path, "abc_photojournalism_nsw.csv")
 done_csv_act = os.path.join(base_path, "abc_photojournalism_act.csv")
@@ -21,6 +21,12 @@ done_csv_nt = os.path.join(base_path, "abc_photojournalism_nt.csv")
 done_csv_sa = os.path.join(base_path, "abc_photojournalism_sa.csv")
 done_csv_wa = os.path.join(base_path, "abc_photojournalism_wa.csv")
 done_csv_tas = os.path.join(base_path, "abc_photojournalism_tas.csv")
+
+done_csv_fire = os.path.join(base_path, "abc_photojournalism_fire.csv")
+done_csv_flood = os.path.join(base_path, "abc_photojournalism_flood.csv")
+done_csv_drought = os.path.join(base_path, "abc_photojournalism_drought.csv")
+done_csv_war = os.path.join(base_path, "abc_photojournalism_war.csv")
+done_csv_solar = os.path.join(base_path, "abc_photojournalism_solar.csv")
 
 shutil.copy(csv_base_path, csv_with_billboards)
 
@@ -33,16 +39,33 @@ sa_lines = []
 wa_lines = []
 tas_lines = []
 
+fire_lines = []
+flood_lines = []
+war_lines = []
+drought_lines = []
+solar_lines = []
+
+goodlines = []
+with open(csv_with_billboards, 'r') as f:
+    goodlines = f.readlines()
+
 with open(csv_with_billboards, 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
 
     i = 0
-    for row in spamreader:
+    for row, goodline in zip(spamreader, goodlines):
+        goodline = goodline.strip()
         state = row[8].strip()
+        keywords = row[10]
         if state == "":
             i = i + 1
             continue
         if i==0:
+            fire_lines.append(",".join(row) + ",Billboards\n")
+            flood_lines.append(",".join(row) + ",Billboards\n")
+            war_lines.append(",".join(row) + ",Billboards\n")
+            drought_lines.append(",".join(row) + ",Billboards\n")
+            solar_lines.append(",".join(row) + ",Billboards\n")
             nsw_lines.append(",".join(row) + ",Billboards\n")
             act_lines.append(",".join(row) + ",Billboards\n")
             nt_lines.append(",".join(row) + ",Billboards\n")
@@ -51,24 +74,36 @@ with open(csv_with_billboards, 'r') as csvfile:
             tas_lines.append(",".join(row) + ",Billboards\n")
             i = i + 1
             continue
-        row_end = ',"images/%s.png"\n' % str(i-1).zfill(4)
+        row_end = ',"images/%s.png"\n' % str(i).zfill(4)
+
+        if keywords.lower().find("war") != -1:
+            war_lines.append(goodline + row_end)
+        if keywords.lower().find("fire") != -1:
+            fire_lines.append(goodline + row_end)
+        if keywords.lower().find("flood") != -1:
+            flood_lines.append(goodline + row_end)
+        if keywords.lower().find("drought") != -1:
+            drought_lines.append(goodline + row_end)
+        if keywords.lower().find("solar") != -1:
+            solar_lines.append(goodline + row_end)
+
         if state == "NSW":
-            nsw_lines.append(",".join(row) + row_end)
+            nsw_lines.append(goodline + row_end)
             continue
         if state == "ACT":
-            act_lines.append(",".join(row) + row_end)
+            act_lines.append(goodline + row_end)
             continue
         if state == "NT":
-            nt_lines.append(",".join(row) + row_end)
+            nt_lines.append(goodline + row_end)
             continue
         if state == "SA":
-            sa_lines.append(",".join(row) + row_end)
+            sa_lines.append(goodline + row_end)
             continue
         if state == "WA":
-            wa_lines.append(",".join(row) + row_end)
+            wa_lines.append(goodline + row_end)
             continue
         if state == "TAS":
-            tas_lines.append(",".join(row) + row_end)
+            tas_lines.append(goodline + row_end)
             continue
         i = i + 1
 
@@ -79,6 +114,12 @@ print("SA: " + str(len(sa_lines)))
 print("WA: " + str(len(wa_lines)))
 print("TAS: " + str(len(tas_lines)))
 
+print("fire: " + str(len(fire_lines)))
+print("war: " + str(len(war_lines)))
+print("flood: " + str(len(flood_lines)))
+print("drought: " + str(len(drought_lines)))
+print("solar: " + str(len(solar_lines)))
+
 writeToFile(done_csv_nsw, nsw_lines)
 writeToFile(done_csv_act, act_lines)
 writeToFile(done_csv_nt, nt_lines)
@@ -86,7 +127,11 @@ writeToFile(done_csv_sa, sa_lines)
 writeToFile(done_csv_wa, wa_lines)
 writeToFile(done_csv_tas, tas_lines)
 
-
+writeToFile(done_csv_fire, fire_lines)
+writeToFile(done_csv_flood, flood_lines)
+writeToFile(done_csv_drought, drought_lines)
+writeToFile(done_csv_war, war_lines)
+writeToFile(done_csv_solar, solar_lines)
 
 
 #for i in range(len(new_lines)):
