dceb53713b6dc4d03a673cbd76c11880bab59c1e
Arran Dengate <arran.dengate@gmail.com>
2015-07-04 13:39:38 +1000

    Got importer working as wanted! But we don't want one big CSV after all. Rewriting to write to many small files.

--- scripts/importer.py
+++ scripts/importer.py
@@ -1,13 +1,11 @@
 __author__ = 'arran'
 
-# Imports the JSON file and does stuff.
+# Imports the CSV file and does stuff.
 
-print("Hello world")
-
-import json
 import urllib2
 import re
-
+import csv
+import sys
 
 def get_word_list(paragraphs):
 
@@ -23,36 +21,55 @@ def get_word_list(paragraphs):
 
 # Import file
 
-file = json.loads(open("data.json").read(), encoding="cp1252")
+# file = json.loads(open("data.json").read(), encoding="cp1252")
 
-with open("output", "w") as outfile:
-    json.dump(file, outfile, indent=4)
+word_data = []
+word_text = ""
 
-# Read each line and go scrape the website.
+with open("output.csv", "w") as output_csv:
 
-for index, line in enumerate(file):
-    url = file[index]["URL"]
-    response = urllib2.urlopen(url)
-    html = response.read()
-    text = re.findall("<p>.*</p>", html)
+    writer = csv.writer(output_csv)
 
-    file[index]["words"] = []
-
-    # Remove HTML tags.
-
-    for item, paragraph in enumerate(text):
-
-        text[item] = re.sub("\<.{1,6}\>", " ", paragraph)      # Remove tags
-        text[item] = re.sub("\<a href=.{1,200}\>", " ", text[item]) # Remove hyperlinks (might del other stuff, could improve)
-
-        #print(file[index]["words"])
-        file[index]["words"].append(text[item])
+    with open('data.csv', 'rb') as input_csv:
+            reader = csv.reader(input_csv, delimiter=',', quotechar="\"")
 
 
+            writer.writerow(reader.next() + ["Words"])
+            #reader.next() # Skip the header row
 
-    file[index]["words"] = get_word_list(file[index]["words"])
 
-    print(file[index]["words"])
+            for index, row in enumerate(reader):
+                if index < 10000: # Was using to limit earlier because it's slow.
+
+                    words = []
+                    url = row[1]
+                    print(url)
+                    response = urllib2.urlopen(url)
+                    html = response.read()
+                    text = re.findall("<p>.*</p>", html)
+
+                    for item, paragraph in enumerate(text):
+
+                        text[item] = re.sub("\<.{1,6}\>", " ", paragraph)      # Remove tags
+                        text[item] = re.sub("\<a href=.{1,200}\>", " ", text[item]) # Remove hyperlinks (might del other stuff, could improve)
+                        words.append(text[item])
+
+                    word_data.append(get_word_list(words))
+
+                    for list in word_data:
+                        for word in list:
+                            word_text = word_text + " " + word
+
+                    #sys.exit()
+
+                word_text = "\"" + word_text + "\""
+                print(word_text)
+                writer.writerow(row + [word_text])
+                word_data = []
+                word_text = ""
+
+
+
 
 
 
