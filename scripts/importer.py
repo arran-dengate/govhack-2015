01f10303d34a29d13f4d2e2c6c75192bbe378f44
Arran Dengate <arran.dengate@gmail.com>
2015-07-04 15:58:50 +1000

    Finally finished the webscrape. See words.csv.

--- scripts/importer.py
+++ scripts/importer.py
@@ -6,6 +6,8 @@ import urllib2
 import re
 import csv
 import sys
+import requests
+from requests.exceptions import HTTPError
 
 def get_word_list(paragraphs):
 
@@ -25,6 +27,7 @@ def get_word_list(paragraphs):
 
 word_data = []
 word_text = ""
+counter = 0
 
 with open("output.csv", "w") as output_csv:
 
@@ -33,19 +36,20 @@ with open("output.csv", "w") as output_csv:
     with open('data.csv', 'rb') as input_csv:
             reader = csv.reader(input_csv, delimiter=',', quotechar="\"")
 
-
             writer.writerow(reader.next() + ["Words"])
             #reader.next() # Skip the header row
 
-
             for index, row in enumerate(reader):
-                if index < 10000: # Was using to limit earlier because it's slow.
-
+                if index < 100000: # Was using to limit earlier because it's slow.
+                    counter += 1
                     words = []
                     url = row[1]
-                    print(url)
-                    response = urllib2.urlopen(url)
-                    html = response.read()
+                    #print(url)
+                    try:
+                        response = urllib2.urlopen(url)
+                        html = response.read()
+                    except urllib2.HTTPError:
+                        html = ""
                     text = re.findall("<p>.*</p>", html)
 
                     for item, paragraph in enumerate(text):
@@ -63,10 +67,11 @@ with open("output.csv", "w") as output_csv:
                     #sys.exit()
 
                 word_text = "\"" + word_text + "\""
-                print(word_text)
+                #print(word_text)
                 writer.writerow(row + [word_text])
                 word_data = []
                 word_text = ""
+                print(counter)
 
 
 
